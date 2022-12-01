4f3b10823a41dd68fbc805fd7340a6b633e3a0f0
ajenkinski <adam@thejenkins.org>
2022-12-01 13:54:01 -0500

    Day 1

---
+++ src/bin/day1.rs
@@ -0,0 +1,32 @@
+// https://adventofcode.com/2022/day/1
+
+use std::fs::read_to_string;
+
+use itertools::Itertools;
+
+fn main() {
+    let input = read_to_string("input/day1-input.txt").unwrap();
+
+    let items_per_elf = input
+        .split("\n\n")
+        .map(|elf_input| {
+            elf_input
+                .lines()
+                .map(|l| l.parse::<u32>().unwrap())
+                .collect_vec()
+        })
+        .collect_vec();
+
+    // Sum calories per elf, and sort in descending order
+    let calories_per_elf = items_per_elf
+        .iter()
+        .map(|items| items.iter().sum::<u32>())
+        .sorted_by(|a, b| Ord::cmp(b, a))
+        .collect_vec();
+
+    let part1_solution = calories_per_elf[0];
+    println!("Part 1 solution = {}", part1_solution);
+
+    let part2_solution = calories_per_elf[0..3].iter().sum::<u32>();
+    println!("Part 2 solution = {}", part2_solution);
+}
