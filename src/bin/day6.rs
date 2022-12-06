76da98b420b29651c35dfb5417c175ab530ffb1a
ajenkinski <adam@thejenkins.org>
2022-12-06 12:23:50 -0500

    Added bit-twiddling Day 6 solution

--- src/bin/day6.rs
+++ src/bin/day6.rs
@@ -19,12 +19,36 @@ fn solve(input: &[char], marker_len: usize) -> usize {
         .expect("Failed to find marker")
 }
 
+// alternate implementation using a bitfield to keep track of unique characters.  I found this technique in the
+// reddit solution thread and liked it. Here's the source: https://github.com/mkeeter/advent-of-code/blob/master/2022/06/src/main.rs
+fn solve_masks(input: &[char], marker_len: usize) -> usize {
+    // mask will contain a 1 bit for every unique character in the last marker_len characters
+    let mut mask: usize = 0;
+    for i in 0..marker_len {
+        mask ^= 1 << input[i] as usize - 'a' as usize;
+    }
+
+    for i in marker_len..input.len() {
+        if mask.count_ones() as usize == marker_len {
+            return i;
+        }
+        mask ^= 1 << input[i] as usize - 'a' as usize;
+        mask ^= 1 << input[i - marker_len] as usize - 'a' as usize;
+    }
+
+    panic!("Failed to find marker");
+}
+
 fn main() {
     let input = read_to_string("input/day6-input.txt")
         .unwrap()
+        .trim()
         .chars()
         .collect_vec();
 
     println!("Part 1 solution = {}", solve(&input, 4));
     println!("Part 2 solution = {}", solve(&input, 14));
+
+    assert_eq!(solve(&input, 4), solve_masks(&input, 4));
+    assert_eq!(solve(&input, 14), solve_masks(&input, 14));
 }
