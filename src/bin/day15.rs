8b6bf7eed71cd0d2a01ade0cf3f08ad8b977c751
ajenkinski <adam@thejenkins.org>
2022-12-16 17:27:13 -0500

    Day 15 part 1

--- src/bin/day15.rs
+++ src/bin/day15.rs
@@ -2,6 +2,8 @@
 
 use std::fs::read_to_string;
 
+use anyhow::{anyhow, Result};
+use itertools::Itertools;
 use regex::Regex;
 
 #[derive(Debug)]
@@ -16,31 +18,33 @@ struct Sensor {
     nearest_beacon: Point,
 }
 
-fn parse_input(input: &String) -> Vec<Sensor> {
-    let sensor_rx = Regex::new(r"Sensor at x=(?P<sx>-?\d+), y=(?P<sy>-?\d+): closest beacon is at x=(?P<bx>-?\d+), y=(?P<by>-?\d+)").unwrap();
+fn parse_input(input: &String) -> Result<Vec<Sensor>> {
+    let sensor_rx = Regex::new(
+        r"Sensor at x=(?P<sx>-?\d+), y=(?P<sy>-?\d+): closest beacon is at x=(?P<bx>-?\d+), y=(?P<by>-?\d+)",
+    )?;
 
     input
         .trim()
         .lines()
-        .map(|line| {
+        .map(|line| -> Result<Sensor> {
             let caps = sensor_rx
                 .captures(line)
-                .expect(format!("Match failed for line {line:?}").as_str());
+                .ok_or(anyhow!("Match failed for line {line:?}"))?;
 
             let location = Point {
-                x: caps["sx"].parse().unwrap(),
-                y: caps["sy"].parse().unwrap(),
+                x: caps["sx"].parse()?,
+                y: caps["sy"].parse()?,
             };
 
             let nearest_beacon = Point {
-                x: caps["bx"].parse().unwrap(),
-                y: caps["by"].parse().unwrap(),
+                x: caps["bx"].parse()?,
+                y: caps["by"].parse()?,
             };
 
-            Sensor {
+            Ok(Sensor {
                 location,
                 nearest_beacon,
-            }
+            })
         })
         .collect()
 }
@@ -49,18 +53,63 @@ fn manhattan_distance(p1: &Point, p2: &Point) -> usize {
     p1.x.abs_diff(p2.x) + p1.y.abs_diff(p2.y)
 }
 
-fn main() {
-    let input = read_to_string("input/day15-input.txt").unwrap();
-    let sensors = parse_input(&input);
+fn solve_part1(sensors: &Vec<Sensor>, target_row: isize) -> usize {
+    // ranges that overlap target_row
+    let mut x_ranges: Vec<(isize, isize)> = vec![];
 
-    for s in sensors.iter() {
-        println!("{s:#?}");
+    for sensor in sensors.iter() {
+        let dist = manhattan_distance(&sensor.location, &sensor.nearest_beacon) as isize;
+
+        if !((sensor.location.y - dist)..=(sensor.location.y + dist)).contains(&target_row)
+            || (sensor.nearest_beacon.y.abs_diff(target_row) as isize == dist)
+        {
+            continue;
+        }
+
+        // calculate the range of row target_row in sensor's area that can't contain a beacon
+        let horizontal_dist = dist - (sensor.location.y.abs_diff(target_row) as isize);
+
+        let mut left = sensor.location.x - horizontal_dist;
+        let mut right = sensor.location.x + horizontal_dist;
+
+        // account of the case where the beacon is on target row
+        if sensor.nearest_beacon.x == left {
+            left += 1;
+        } else if sensor.nearest_beacon.x == right {
+            right -= 1;
+        }
+
+        x_ranges.push((left, right));
     }
 
-    let sensor = &sensors[6];
-    println!(
-        "Distance for {:#?} is {}",
-        sensor,
-        manhattan_distance(&sensor.location, &sensor.nearest_beacon)
+    // now calculate how many unique elements of target_row are covered by x_ranges
+
+    // first merge overlapping ranges
+    let merged_ranges = x_ranges.into_iter().sorted_by_key(|r| r.0).fold(
+        vec![],
+        |mut merged, range @ (left, right)| {
+            if let Some((_, prev_right)) = merged.last_mut() {
+                if *prev_right >= left {
+                    *prev_right = right.max(*prev_right);
+                } else {
+                    merged.push(range);
+                }
+            } else {
+                merged.push(range);
+            }
+            merged
+        },
     );
+
+    merged_ranges
+        .into_iter()
+        .map(|(left, right)| (right - left + 1) as usize)
+        .sum()
+}
+
+fn main() {
+    let input = read_to_string("input/day15-input.txt").unwrap();
+    let sensors = parse_input(&input).unwrap();
+
+    println!("Part 1 solution = {}", solve_part1(&sensors, 2000000));
 }
